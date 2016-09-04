25363620428614bebdfe59f59b75432319c2d8a8
DESKTOP-CG15MBB\Owner <smarttcc@gmail.com>
2016-09-04 14:52:21 -0400

    Removing the sample code.

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
 "use strict";
-const lib_1 = require('./lib');
-const fs = require('fs');
-const builder = new lib_1.ChainBuilder();
-builder.then((data, next, error) => {
-    fs.readFile('./index.ts', 'UTF-8', (err, data) => {
-        if (err == null) {
-            next(data);
-        }
-        else {
-            error(err);
-        }
-    });
-}).then((data, next, error) => {
-    console.log(data.toString());
-    next(data);
-}).then((data, next, error) => {
-    fs.writeFile('./clone.goofy', data, (err) => {
-        if (err == null) {
-            next();
-        }
-        else {
-            error(err);
-        }
-    });
-});
-builder.on('done', () => {
-    console.log('we are done!');
-});
-builder.on('error', (err) => {
-    console.log('error - detail');
-    console.log(err);
-});
-builder.execute();
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./lib'));
