a6a8775271f7e7320d3df1cd21b5fd093eb53294
DESKTOP-CG15MBB\Owner <smarttcc@gmail.com>
2016-09-04 15:14:46 -0400

    Making the data arguments optional for next and error.

--- lib/lib.class.chainbuilder.ts
+++ lib/lib.class.chainbuilder.ts
@@ -2,20 +2,20 @@ import {ChainItem} from './lib.class.chainitem'
 import {EventEmitter} from 'events';
 
 export class ChainBuilder extends EventEmitter {
-    public stack:ChainItem[] = [];
+    public stack: ChainItem[] = [];
 
-    public push(value : ChainItem):ChainBuilder {
+    public push(value: ChainItem): ChainBuilder {
         this.stack.push(value);
         return this;
     }
 
-    public next(data : any):void {
-        const self : ChainBuilder = this;
+    public next(data?: any): void {
+        const self: ChainBuilder = this;
 
-        if(self.stack.length > 0){
-            self.stack.shift().next(data, (data: any) => {
+        if (self.stack.length > 0) {
+            self.stack.shift().next(data, (data?: any) => {
                 self.next(data);
-            }, (data : any) => {
+            }, (data?: any) => {
                 self.error(data)
             })
         } else {
@@ -23,15 +23,15 @@ export class ChainBuilder extends EventEmitter {
         }
     }
 
-    public error(data : any):void {
+    public error(data?: any): void {
         this.emit('error', data);
     }
 
-    public then(callback : (data : any, next : (data : any) => void, error : (data : any) => void) => void):ChainBuilder{
+    public then(callback: (data: any, next: (data?: any) => void, error: (data?: any) => void) => void): ChainBuilder {
         return this.push(new ChainItem(callback));
     }
 
-    public execute(data?:any):void {
+    public execute(data?: any): void {
         this.next(data || null);
     }
 }
--- lib/lib.class.chainitem.ts
+++ lib/lib.class.chainitem.ts
@@ -2,13 +2,13 @@
  * Items to be chained together with a next and error function.
  */
 export class ChainItem {
-    private action:(data : any, next:(data : any) => void, error:(data:any) => void) => void;
+    private action: (data: any, next: (data?: any) => void, error: (data?: any) => void) => void;
 
-    public next(data : any, next:(data : any) => void, error:(data : any) => void):void{
+    public next(data: any, next: (data?: any) => void, error: (data?: any) => void): void {
         this.action(data, next, error);
     }
 
-    constructor(action : (data : any, next:(data : any) => void, error:(data:any) => void) => void){
+    constructor(action: (data: any, next: (data?: any) => void, error: (data?: any) => void) => void) {
         this.action = action;
     }
 }
